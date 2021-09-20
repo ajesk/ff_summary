210ab3105ce3e2688d23b8bcc5e3ce6f1ce2acfc
Aaron Jeskie <aaronjeskie@gmail.com>
2021-09-19 21:04:00 -0400

    Updates

---
+++ precache-manifest.f740324888c47a7292d960e23ab8cf0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f00126b4ceb8ab1595d",
+    "url": "/ff_summary/static/css/main.342e12d9.chunk.css"
+  },
+  {
+    "revision": "4f00126b4ceb8ab1595d",
+    "url": "/ff_summary/static/js/main.4f00126b.chunk.js"
+  },
+  {
+    "revision": "cce081fd9611a1c7b771",
+    "url": "/ff_summary/static/js/1.cce081fd.chunk.js"
+  },
+  {
+    "revision": "e7906696fc1b9498cbf8",
+    "url": "/ff_summary/static/js/runtime~main.e7906696.js"
+  },
+  {
+    "revision": "fb1a88c520e36eee89f7d6c947759f20",
+    "url": "/ff_summary/index.html"
+  }
+];
